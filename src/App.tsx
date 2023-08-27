6751977f07fb5eb77158743b0938a980e2eb9cab
Ogwu Emmanuel Berit <beritogwu@gmail.com>
2023-08-27 22:22:58 +0100

    work on route implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { useAppDispatch, useAppSelector } from "./store/store";
-import { addMovie } from "./store/features/movieSlice";
-// import { Notifications } from "@mantine/notifications";
+import HomeLayout from "./layouts/HomeLayout";
+import HomePage from "./pages/HomePage";
+import AddMovieComponent from "./pages/AddMovie";
+import MovieDetails from "./pages/MovieDetails";
+import NoMatch from "./pages/NotMatch";
 
 function App() {
-	const dispatch = useAppDispatch();
-	const { movies } = useAppSelector((store) => store.movie);
-	dispatch(addMovie);
-	console.log(movies);
-
-	return <>{/* <Notifications /> */}</>;
+	return (
+		<Routes>
+			<Route path="/" element={<HomeLayout />}>
+				<Route index element={<HomePage />} />
+				<Route path="/add-movie" element={<AddMovieComponent />} />
+				<Route path="/movie-details/:id" element={<MovieDetails />} />
+				<Route path="*" element={<NoMatch />} />
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,41 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage/index.tsx";
-import HomeLayout from "./layouts/HomeLayout.tsx";
-import AddMovieComponent from "./pages/AddMovie/index.tsx";
-import MovieDetails from "../src/pages/MovieDetails";
+import { BrowserRouter } from "react-router-dom";
 import { Notifications } from "@mantine/notifications";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <HomeLayout />,
-		children: [
-			{
-				path: "",
-				element: <HomePage />,
-			},
-			{
-				path: "/add-movie",
-				element: <AddMovieComponent />,
-			},
-			{
-				path: "/movie-details/:id",
-				element: <MovieDetails />,
-			},
-		],
-	},
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<Provider store={store}>
 		<React.StrictMode>
-			<RouterProvider router={router}>
+			<BrowserRouter>
 				<Notifications />
 				<App />
-			</RouterProvider>
+			</BrowserRouter>
 		</React.StrictMode>
 	</Provider>
 );
